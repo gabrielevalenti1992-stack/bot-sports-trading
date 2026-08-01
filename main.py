ad8d91c348c415ecd446e60dc431e9e788eaa68a
gabrielevalenti1992-stack <gabriele.valenti.1992@gmail.com>
2026-08-01 22:53:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     self.end_headers()
     self.wfile.write(b"Bot Online!")
 
-    def do_HEAD(self):
+        def do_HEAD(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
         self.end_headers()
+
 
 
 def run_web_server():
